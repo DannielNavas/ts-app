7705e63e598220f96f4db438f522a15fc75436ef
Danniel Navas <dannielnavas@gmail.com>
2023-02-20 05:57:43 -0500

    ✨ herencia en interfaces

--- src/app/users/user.model.ts
+++ src/app/users/user.model.ts
@@ -1,10 +1,12 @@
+import { BaseModel } from '../base.model';
+
 export enum ROLES {
   ADMIN = 'admin',
   SALLER = 'saller',
   COSTUMER = 'costumer',
 }
 
-export interface User {
+export interface User extends BaseModel {
   username: string | number;
   role: ROLES;
 }
