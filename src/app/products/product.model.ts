88769b9936c85ebaea12e00ec7c7e3b7e5d18e86
Luis Daniel Gordo Navas <luis.gordo@soytul.com>
2023-02-21 06:03:16 -0500

    ✨ creation with faker data

--- src/app/products/product.model.ts
+++ src/app/products/product.model.ts
@@ -5,7 +5,13 @@ type Size = 'small' | 'medium' | 'large';
 export type userId = string | number;
 export interface Product extends BaseModel {
   title: string;
+  image: string;
+  description: string;
+  price: number;
+  color: string;
   stock: number;
   size?: Size;
   category: Category;
+  isNew: boolean;
+  tags: string[];
 }
--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -5,3 +5,31 @@ export const products: Product[] = [];
 export const addProduct = (data: Product) => {
   products.push(data);
 };
+
+export const updatedProduct = (id: string, changes: Product) => {
+  // code
+};
+
+export const deleteProduct = (id: string) => {
+  // code
+};
+
+export const getProduct = (id: string) => {
+  // code
+};
+
+export const getProducts = () => {
+  // code
+};
+
+export const getProductsByCategory = (categoryId: string) => {
+  // code
+};
+
+export const getProductsByTag = (tag: string) => {
+  // code
+};
+
+export const getProductsBySize = (size: string) => {
+  // code
+};
