e69b06095b2df250e1592d2c1fa14347349a6563
Danniel Navas <dannielnavas@gmail.com>
2023-02-21 06:03:16 -0500

    ✨ creation with faker data

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,15 +1,27 @@
-import { addProduct } from './products/product.service';
-addProduct({
-  id: 1,
-  title: 'Camisa',
-  createAt: new Date(),
-  stock: 10,
-  size: 'small',
-  category: {
-    id: 1,
-    createAt: new Date(),
-    updateAt: new Date(),
-    name: 'Ropa',
-  },
-  updateAt: new Date(),
-});
+import { faker } from '@faker-js/faker';
+import { addProduct, products } from './products/product.service';
+
+for (let index = 0; index < 50; index++) {
+  addProduct({
+    id: faker.datatype.uuid(),
+    title: faker.commerce.productName(),
+    createAt: faker.date.recent(),
+    stock: faker.datatype.number({ min: 10, max: 100 }),
+    size: faker.helpers.arrayElement(['small', 'medium', 'large']),
+    category: {
+      id: faker.datatype.uuid(),
+      createAt: faker.date.recent(),
+      updateAt: faker.date.recent(),
+      name: faker.commerce.department(),
+    },
+    updateAt: faker.date.recent(),
+    image: faker.image.imageUrl(),
+    description: faker.commerce.productDescription(),
+    price: Number(faker.commerce.price()),
+    color: faker.color.human(),
+    isNew: faker.datatype.boolean(),
+    tags: faker.helpers.arrayElements(),
+  });
+}
+
+console.log(products);
