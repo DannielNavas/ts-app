0c120730fe585cdc877946c9b14991bcdbceee5e
Luis Daniel Gordo Navas <luis.gordo@soytul.com>
2023-02-23 06:03:47 -0500

    ✨ Omit and pick

--- src/app/main.ts
+++ src/app/main.ts
@@ -3,18 +3,10 @@ import { addProduct, products } from './products/product.service';
 
 for (let index = 0; index < 50; index++) {
   addProduct({
-    id: faker.datatype.uuid(),
+    categoryId: faker.datatype.uuid(),
     title: faker.commerce.productName(),
-    createAt: faker.date.recent(),
     stock: faker.datatype.number({ min: 10, max: 100 }),
     size: faker.helpers.arrayElement(['small', 'medium', 'large']),
-    category: {
-      id: faker.datatype.uuid(),
-      createAt: faker.date.recent(),
-      updateAt: faker.date.recent(),
-      name: faker.commerce.department(),
-    },
-    updateAt: faker.date.recent(),
     image: faker.image.imageUrl(),
     description: faker.commerce.productDescription(),
     price: Number(faker.commerce.price()),
