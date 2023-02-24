57050a706a035032d0a7d27bbec7fe565743d333
Danniel Navas <dannielnavas@gmail.com>
2023-02-24 05:59:47 -0500

    ✨ Readonly, Partial, Pick, Required

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,5 +1,10 @@
 import { faker } from '@faker-js/faker';
-import { addProduct, products } from './products/product.service';
+import {
+  addProduct,
+  findProducts,
+  products,
+  updatedProduct,
+} from './products/product.service';
 
 for (let index = 0; index < 50; index++) {
   addProduct({
@@ -17,3 +22,15 @@ for (let index = 0; index < 50; index++) {
 }
 
 console.log(products);
+const product = products[0];
+updatedProduct(product.id, {
+  title: 'New title',
+  stock: 80,
+  price: 101,
+});
+
+findProducts({
+  stock: 10,
+  color: 'red',
+  isNew: true,
+});
--- src/app/products/product.dto.ts
+++ src/app/products/product.dto.ts
@@ -6,3 +6,11 @@ export interface CreateProductDto
 }
 // TODO: elijo los campos que quiero que sean obligatorios
 type example = Pick<Product, 'color' | 'description'>;
+// TODO: Deja todo opcional
+export interface UpdateProductDto extends Partial<CreateProductDto> {}
+// TODO: Deja todo obligatorio
+type example2 = Required<Product>;
+// TODO: Deja una interface PArcial pero solo de lectura no se puede sobre escribir el parametro
+export interface FindProductDto extends Readonly<Partial<Product>> {}
+
+// type example2 = Readonly<Product>;
